39ce3c547281eae3a720b3c5c18afdbfa5241c2d
huss2012 <huss24940@gmail.com>
2024-10-02 20:22:18 +0300

    configure postgres database.

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,10 +2,17 @@ const config = require('./index');
 
 module.exports = {
   development: {
-    storage: config.dbFile,
-    dialect: "sqlite",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
     seederStorage: "sequelize",
     logQueryParameters: true,
-    typeValidation: true
+    typeValidation: true,
+    dialectOptions: {
+      useUTC: true,
+      timezone: 'Z'
+    }
   }
 };
--- backend/config/index.js
+++ backend/config/index.js
@@ -1,5 +1,11 @@
 module.exports = {
   environment: process.env.NODE_ENV || "development",
   port: process.env.PORT || 5000,
-  dbFile: process.env.DB_FILE || 'db/dev.db'
+  db: {
+    username: process.env.DB_USERNAME || 'hussain',
+    password: process.env.DB_PASSWORD || 'Hussain',
+    database: process.env.DB_NAME || 'articles_db',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres'
+  }
 };
